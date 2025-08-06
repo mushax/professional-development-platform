9840d49e04254ee0a091d8d837827bfb08490c7f
mushax <bitfares@gmail.com>
2025-08-07 02:47:24 +0300

    Feat: Implement authentication scaffolding using Laravel Breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,44 +11,11 @@ export default {
 
     theme: {
         extend: {
-            // تعريف ألوان الهوية البصرية للمشروع
-            colors: {
-                'primary': {
-                    '50': '#eff6ff',
-                    '100': '#dbeafe',
-                    '200': '#bfdbfe',
-                    '300': '#93c5fd',
-                    '400': '#60a5fa',
-                    '500': '#3b82f6', // اللون الأساسي
-                    '600': '#2563eb',
-                    '700': '#1d4ed8',
-                    '800': '#1e40af',
-                    '900': '#1e3a8a',
-                    '950': '#172554',
-                },
-                'secondary': {
-                    '50': '#f8fafc',
-                    '100': '#f1f5f9',
-                    '200': '#e2e8f0',
-                    '300': '#cbd5e1',
-                    '400': '#94a3b8',
-                    '500': '#64748b', // اللون الثانوي
-                    '600': '#475569',
-                    '700': '#334155',
-                    '800': '#1e293b',
-                    '900': '#0f172a',
-                    '950': '#020617',
-                }
-            },
-            // تعريف الخطوط التي سيتم استخدامها (يمكن تعديلها لاحقاً)
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans],
-                // arabic: ['Tajawal', 'sans-serif'],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [
-        forms
-    ],
+    plugins: [forms],
 };
